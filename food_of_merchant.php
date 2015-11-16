ba77c5c675bb330429793d5f603dd54488a801c7
chongdeng <fqyyang@gmail.com>
2015-11-16 16:55:37 +0800

    finish merchant review food; finish merchant delete food

---
+++ food_of_merchant.php
@@ -0,0 +1,74 @@
+<?php  
+   include_once('food_galaxy_fns.php'); 
+   do_html_header("Food of Merchant:");
+   
+  
+   
+   if($_GET['delete_food_id']){
+   	 if(!delete_food($_GET['delete_food_id']))
+   	 	echo "Error: Failed to delete food";
+   	 
+   	 	
+   }
+   
+    display_food_of_merchant();
+  
+?>
+
+
+
+<?php 
+   do_html_footer();
+?>
+
+<?php
+	
+	function delete_food($food_id){
+		require_once ('food_galaxy_fns.php');
+		
+		$conn = db_connect();
+		$quey = null; $result = null;
+		$merchant_id = $_GET['merchant_id'];	
+		$query = "delete from food where food_id = '".$food_id."'";
+		$result = @$conn->query($query);
+	   	if (!$result){
+	   		echo  "Error: Can't delete food from database";
+	   		return false;
+	   	}
+	   	$filename = "img/".$food_id.".jpg";
+	   	unlink($filename);
+		return true;
+	} 
+	
+	function display_food_of_merchant(){
+		require_once ('food_galaxy_fns.php');
+			
+		$conn = db_connect();
+		$quey = null; $result = null;
+		$merchant_id = $_GET['merchant_id'];	
+		$query = "select food_id from food where merchant_id = '".$merchant_id."'";
+		$result = @$conn->query($query);
+	   	if (!$result) echo  "Error: Can't execute query about food list of merchant";
+	   	while($row = $result->fetch_assoc()){   			
+	   			echo "<div class=\"col-xs-12 col-sm-6 col-md-4\">
+				  <div class=\"thumbnail\">
+				    <a href=\"merchant_food_details.php?food_id=".$row['food_id']."\"><img src=\"img/".$row['food_id'].".jpg\" alt=\"...\"></a>
+					<div class=\"caption\">
+					  <h3>".$row['name']."</h3>
+					  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
+					   Donec id elit non mi porta gravida at eget metus.
+					   Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					   <p>
+					      <a href=\"merchant_food_details.php?food_id=".$row['food_id']."\" class=\"btn btn-primary\" role=\"button\">View & Edit</a> 
+					      <a href=\"food_of_merchant.php?merchant_id=".$merchant_id."&delete_food_id=".$row['food_id']."\" class=\"btn btn-primary\" role=\"button\">Delete this food</a>				     
+					   </p>
+				      </div>
+				  </div>
+				</div>";
+			  	array_shift($food_array);
+	   	}
+	}
+?>
+
+
+ 
--- galaxy_fns.php
+++ galaxy_fns.php
@@ -44,12 +44,9 @@ function display_foods_of_category($food_array) {
   	if (!is_array($food_array)) {
     	echo "<p>No Foods currently available in this category</p>";
   	} 
-  	else{
-		while(count($food_array) >= 3){
-	  		$obj1 = $food_array[0];
-	  		$obj2 = $food_array[1];
-	  		$obj3 = $food_array[2];
-	  		//echo $obj1['food_id'].$obj2['food_id'].$obj3['food_id'];
+  	else{  		
+		while(count($food_array)){
+	  		$obj1 = $food_array[0];	  		
 	  	
 		  	echo "<div class=\"col-xs-12 col-sm-6 col-md-4\">
 			  <div class=\"thumbnail\">
@@ -65,43 +62,10 @@ function display_foods_of_category($food_array) {
 				   </p>
 			      </div>
 			  </div>
-			</div>
-			
-			<div class=\"col-xs-12 col-sm-6 col-md-4\">
-			  <div class=\"thumbnail\">
-			    <a href=\"food_details.php?food_id=".$obj2['food_id']."\"><img src=\"img/".$obj2['food_id'].".jpg\" alt=\"...\"></a>
-				<div class=\"caption\">
-				  <h3>".$obj2['name']."</h3>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
-				   Donec id elit non mi porta gravida at eget metus.
-				   Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				   <p>
-				      <a href=\"food_details.php?food_id=".$obj2['food_id']."\" class=\"btn btn-primary\" role=\"button\">View Details</a> 
-				      <a href=\"#\" class=\"btn btn-default\" role=\"button\">Button</a>				     
-				   </p>
-			     </div>
-			  </div>
-			</div>
-			<div class=\"col-xs-12 col-sm-6 col-md-4\">
-			  <div class=\"thumbnail\">
-			    <a href=\"food_details.php?food_id=".$obj3['food_id']."\"><img src=\"img/".$obj3['food_id'].".jpg\" alt=\"...\"></a>
-				<div class=\"caption\">
-				  <h3>".$obj3['name']."</h3>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
-				   Donec id elit non mi porta gravida at eget metus.
-				   Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				   <p>
-				      <a href=\"food_details.php?food_id=".$obj3['food_id']."\" class=\"btn btn-primary\" role=\"button\">View Details</a> 
-				      <a href=\"#\" class=\"btn btn-default\" role=\"button\">Button</a>				     
-				   </p>
-			     </div>
-			  </div>
 			</div>";
-		  	array_shift($food_array);	
 		  	array_shift($food_array);
-		  	array_shift($food_array);
-	  	}	
-  	}
+		}		  	
+	 }	  
   	echo "<hr />";
 }
 
